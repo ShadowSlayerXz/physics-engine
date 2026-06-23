99251a1095c923821d35309312da4a9ee9b6df29
ShadowSlayerXz <xz9coder@gmail.com>
2026-06-23 18:43:02 +0530

    Added the main.cpp and included the needed libraries

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+constexpr float GRAVITY = 9.81f;
+constexpr float RESTITUTION = 0.8f;
+constexpr float DT = 1.0f/60.0f;
+constexpr float WINDOW_WIDTH = 800.0f;
+constexpr float WINDOW_HEIGHT = 600.0f;
+
+int main() {
+   cout << "testing" << endl;
+   return 0;
+}
